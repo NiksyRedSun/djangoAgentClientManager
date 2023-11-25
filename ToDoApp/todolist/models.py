bf637e4b9bffeadf9339dd82596e2a1f2d412e36
NiksyRedSun <112153281+NiksyRedSun@users.noreply.github.com>
2023-11-25 22:19:02 +0300

    Initial commit

---
+++ ToDoApp/todolist/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Tasks(models.Model):
+    title = models.CharField('Название задания', max_length=500)
+    is_complete = models.BooleanField('Завершено', default=False)
+
+
+    class Meta:
+        verbose_name = 'Задание'
+        verbose_name_plural = 'Задания'
+
+
+    def __str__(self):
+        return self.title
