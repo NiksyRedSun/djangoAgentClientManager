ad1394f4f26c83beee41559ccb3c9c086bd4332d
NiksyRedSun <112153281+NiksyRedSun@users.noreply.github.com>
2023-11-26 22:00:08 +0300

    NoCss

--- ToDoApp/todolist/urls.py
+++ ToDoApp/todolist/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('add', views.add, name='add'),
+    path('update/<int:todo_id>', views.update, name='update'),
+    path('delete/<int:todo_id>', views.delete, name='delete'),
 ]
--- ToDoApp/todolist/views.py
+++ ToDoApp/todolist/views.py
@@ -1,9 +1,44 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from .models import Tasks
+
+from django.views.decorators.http import require_http_methods
+
+from django.contrib import messages
+
+
+
 
 # Create your views here.
 
 
 
 def index(request):
-    return HttpResponse("Hello")
+    todos = Tasks.objects.all()
+    return render(request, "ToDoApp/index.html", {'todo_list': todos, 'title': "Главная страница"})
+
+
+@require_http_methods(['POST'])
+def add(request):
+    title = request.POST['title']
+    if not title:
+        messages.error(request, 'Задача не может быть пустой')
+    else:
+        todo = Tasks(title=title)
+        todo.save()
+        messages.success(request, 'Успешно сохранено')
+    return redirect('index')
+
+
+def update(request, todo_id):
+    todo = Tasks.objects.get(id=todo_id)
+    todo.is_complete = not todo.is_complete
+    todo.save()
+    return redirect('index')
+
+
+def delete(request, todo_id):
+    todo = Tasks.objects.get(id=todo_id)
+    todo.delete()
+    return redirect('index')
